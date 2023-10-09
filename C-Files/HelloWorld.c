2048196011e56c434e4d3046c8ad0176950d44f2
vhast <fedvhast@gmail.com>
2023-10-10 00:49:11 +0300

    First commit

---
+++ C-Files/HelloWorld.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    // This is a comment
+
+    printf("Is really good!\n");
+
+    printf("\"I like Pizza\" - Abraham Lincoln");
+    return 0;
+}
---
+++ C-Files/Variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int x; // Declaration
+    x = 123; // Initialization
+
+    int y = 321; //Declaration + Initialization
+
+    int age = 21; // Integer (whole number)
+    float gpa = 2.05; // Floating point number (contains a decimal portion)
+    char grade = 'C'; // Single character
+    char name[] = "Bro"; // Array of characters
+
+    return 0;
+}
